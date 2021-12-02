50e468fa584c33fef0deb94fab46616a9faae23b
davealexis <dalexis@gmail.com>
2021-12-02 18:04:32 -0500

    Cleaned up the puzzle 2 Nim code a bit

--- puzzle2/puzzle2a.nim
+++ puzzle2/puzzle2a.nim
@@ -2,30 +2,34 @@ import std/strformat, strutils
 
 #..............................................................................
 proc runSubmarine() =
-    var position = 0
-    var depth = 0
+    var
+        position = 0
+        depth = 0
+        line: string
 
     let f = open("./input.txt")
     defer: f.close()
 
-    var line: string
-
     while f.readLine(line):
-        var s = line.strip().split(' ')
-        var direction = s[0]
-        var distance = parseInt(s[1])
-        echo(&"{direction} {distance}")
+        var
+            s = line.strip().split(' ')
+            direction = s[0]
+            distance = parseInt(s[1])
 
         case direction:
             of "forward":
                 position += distance
+
             of "up":
                 depth -= distance
 
                 if depth < 0:
                     echo("BREACH!!")
+
             of "down":
                 depth += distance
+
+        echo(&"{direction} {distance}")
 
     var hp = position * depth
     echo(&"position: {position}, depth: {depth}. total: {hp}")
--- puzzle2/puzzle2b.nim
+++ puzzle2/puzzle2b.nim
@@ -1,38 +1,42 @@
 import std/strformat, strutils
 
-
-var position = 0
-var depth = 0
-var aim = 0
-
+#..............................................................................
 proc runSub() =
     let f = open("./input.txt")
     defer: f.close()
 
-    var line: string
+    var
+        position = 0
+        depth = 0
+        aim = 0
+        line: string
 
     while f.readLine(line):
-        var s = line.strip().split(' ')
-        var direction = s[0]
-        var distance = parseInt(s[1])
-        echo(&"{direction} {distance}")
+        var
+            s = line.strip().split(' ')
+            direction = s[0]
+            distance = parseInt(s[1])
 
         case direction:
             of "forward":
                 position += distance
                 depth += aim * distance
+
             of "up":
                 # depth -= distance
                 aim -= distance
 
                 if depth < 0:
                     echo("BREACH!!")
+
             of "down":
                 # depth += distance
                 aim += distance
 
+        echo(&"{direction} {distance} -->> Aim: {aim}  Position: {position} Depth: {depth}")
+
     var hp = position * depth
-    echo(&"position: {position}, depth: {depth}, aim: {aim}. total: {hp}")
+    echo(&"\nposition: {position}, depth: {depth}, aim: {aim}. total: {hp}")
 
-
+#..............................................................................
 runSub()
